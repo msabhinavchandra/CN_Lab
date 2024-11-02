1d2ab690b248b36d0347292f56415343f91941ae
M.S.Abhinav Chandra <116635275+msabhinavchandra@users.noreply.github.com>
2024-11-02 17:29:50 +0530

    Sundeep

---
+++ Flow_Control_Sundeep.c
@@ -0,0 +1,30 @@
+/*Develop a simple data link layer that performs the flow control using the Go Back N protocol in   “c”
+
+Sample
+
+*/
+#include<stdio.h>
+int main()
+{
+               int window=0;
+	printf("enter Window size : ");
+	scanf("%d",&window);
+	int sent,ack,i=0;
+	while(1){
+		for(i=0;i<window;i++){
+			printf("frame Transmitted %d \n",sent);
+			sent++;
+			if(sent==window){
+				break;
+			}
+		}
+		printf("enter last received acknowledgment : ");
+		scanf("%d",&ack);
+		if(ack==window){
+			break;
+		}
+		else{
+			sent = ack;
+			}
+	}
+}
